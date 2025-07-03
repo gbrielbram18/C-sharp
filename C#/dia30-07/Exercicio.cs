9b8bd1d5e66cb716363beea7f3c3e8eac54e7dea
gbrielbram18 <biel300816@gmail.com>
2025-07-03 14:20:49 -0300

    adicionando atividade

---
+++ C#/dia30-07/Exercicio.cs
@@ -0,0 +1,11 @@
+int idade = 18;
+bool maiorDeIdade = idade > 18;
+
+if (maiorDeIdade == true)
+{
+    Console.WriteLine("voce e adulto");
+}
+else
+{
+    Console.WriteLine("vc nao e audlto");   
+}    
---
+++ C#/dia30-07/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+/*
+
+//= operador de atribuição de valor 
+string operacao = "Soma"; // Declaração de variável do tipo string
+int numA = 5; // Declarção de variável do tipo int
+int numB = 12;
+Console.WriteLine(numA + numB);
+
+// Console.WriteLine() - Instrução para escrita de mensagens do console
+Console.WriteLine("A operação que está sendo realizada é " + operacao);
+Console.WriteLine(numA + numB);
+
+string mult = "mult"; // Declaração de variável tipo int
+int numC = 4;
+int numD = 4;
+Console.WriteLine("numC * numD");
+//Console.WriteLine() - Instrução para escrita de mensagens do console
+Console.WriteLine("A operação que está sendo realizada é " + mult);
+Console.WriteLine(numC * numD);
+*/
+
