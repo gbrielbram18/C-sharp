9441cee3c68672a7e67e1b52a46c43173d7589c9
gbrielbram18 <biel300816@gmail.com>
2025-07-07 13:46:01 -0300

    adicionando atividades em suas devidas pastas

---
+++ C#/dia30-07/exercios/atividade3/Program.cs
@@ -0,0 +1,17 @@
+﻿//exercicio 3.1
+
+
+/*
+Escreva um programa que imprima os números de 1 a 10, um em cada linha.
+*/
+
+
+
+while (contador <= 10)
+{
+    Console.WriteLine(contador);
+    contador++;
+}
+
+
+
