f464b1e3f12643fabeb16308760f84ffd86aac0e
gbrielbram18 <biel300817@gmail.com>
2025-07-14 14:32:40 -0300

    adicionando alterações e nova atividade

---
+++ C#/dia30-07/exercios/caixa_eletronico/Program.cs
@@ -0,0 +1,35 @@
+﻿double saldo =0 ;
+int opcao;
+
+do{
+    ExibirMenu();
+
+    //opção == int.Parse (Console.Readline());
+    if (int.TryParse(Console.Readline(),out opcao))
+    {
+        
+        Console.WriteLine($"Opção selecionada {opcao}");
+    }
+    else
+    {
+        Console.WriteLine($"a {opcao} Não e uma opção valida, tente novamente.");
+        Console.ReadKey();
+    }
+    
+} while (opcao !=5);
+
+ExibirMenu(){
+Console.WriteLine("Selecione uma das opções abaixo: ");
+Console.WriteLine(" 1 - Saldo ");
+Console.WriteLine(" 2 -   ");
+Console.WriteLine(" 3 - ");
+Console.WriteLine(" 4  ");
+Console.WriteLine(" 5 ");
+Console.WriteLine(" 6  ");
+Console.WriteLine(" 7  ");
+Console.WriteLine(" 8  ");
+Console.WriteLine(" 9 ");
+
+
+
+}
