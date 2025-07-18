7059ffab7ec90927da784a9c2ae224f74053f7e1
gbrielbram18 <biel300817@gmail.com>
2025-07-18 15:34:33 -0300

    ultimo dia

---
+++ C#/dia30-07/exercios/ultimoDIa/ativ1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ativ1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        Retangulo calculo = new Retangulo();
+       
+
+
+
+    
+    }
+}
---
+++ C#/dia30-07/exercios/ultimoDIa/ativ1/Retangulo.cs
@@ -0,0 +1,24 @@
+namespace ativ1;
+
+
+
+class Retangulo {
+    
+    
+    public int base;
+    public int altura;
+    
+
+    public void Calculo(){
+        Console.WriteLine("digite o valor da base: ");
+        Console.ReadLine()
+        Console.WriteLine("Digite o valor da alura: ");
+        Console.ReadLine();
+        Console.WriteLine($" a soma dos calculos de {resultado} e: ");
+        
+    
+    
+
+
+    }
+}
